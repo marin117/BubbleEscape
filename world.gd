b8ef48e9965fd108365c675c6782f5623406fd73
marin <croatiathbest@gmail.com>
2025-01-25 19:04:13 +0100

    Base level created

--- world.gd
+++ world.gd
@@ -3,9 +3,4 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_tree().change_scene_to_file("res://Level1.tscn")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	get_tree().change_scene_to_file("res://BaseLevel.tscn")
