b79ed5dc2840db6a94f9858927ac8c784a3001ef
marin <croatiathbest@gmail.com>
2025-01-25 14:52:53 +0100

    hands and animations update

--- player.gd
+++ player.gd
@@ -12,8 +12,6 @@ const Bubble = preload( "res://bubble.gd")
 
 
 @export var state : Types.PlayerStates = Types.PlayerStates.IDLE
-@export var flipped: int = 1
-@export var weapon_rotation: float = 0.0
 @export var weapon_marker_position: Vector2 = Vector2(0,0)
 
 func _apply_forces(delta: float) -> void:
@@ -46,14 +44,16 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	var mouse_pos: Vector2 = get_global_mouse_position()
+
 	var angle_to_mouse: float = rad_to_deg((mouse_pos - $Weapon.global_position).normalized().angle())
 	if angle_to_mouse < 0:  
 		angle_to_mouse += 360
-	if angle_to_mouse < 20 or angle_to_mouse > 160:
-		$Weapon.look_at(get_global_mouse_position())
-	$Sprite2D.flip_h = position > get_global_mouse_position()
-	weapon_rotation = $Weapon.global_rotation
-	flipped = -1 if $Sprite2D.flip_h else 1
+	if ((angle_to_mouse < 20 or angle_to_mouse > 160) and 
+		(angle_to_mouse < 240 or angle_to_mouse > 300)):
+		$Body.flip_h = position > mouse_pos
+		$Hands.flip_v = position > mouse_pos
+		$Weapon.look_at(mouse_pos)
+		$Hands.look_at(mouse_pos)
 	weapon_marker_position = $Weapon/WeaponMarker.global_position
 	_change_animation()
 	move_and_slide()
