6994fe1d659538eed213475ae4ce4e75b7480c0a
marin <croatiathbest@gmail.com>
2025-01-25 18:35:11 +0100

    Door death collider fix

--- door.gd
+++ door.gd
@@ -2,10 +2,22 @@ extends Area2D
 
 signal door_opened
 
+var time_passed : int = 0
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("players"):
 		$AnimationPlayer.play("open")
-		$OpenDoorTimer.start(3)
+		$OpenDoorTimer.start(1)
 
 func _on_open_door_timer_timeout() -> void:
-	door_opened.emit()
+	$OpenDoorTimer.start(1)
+	time_passed += 1
+	if time_passed == 3:
+		door_opened.emit()
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("players"):
+		$OpenDoorTimer.stop()
+		$AnimationPlayer.play("RESET")
+		time_passed = 0
