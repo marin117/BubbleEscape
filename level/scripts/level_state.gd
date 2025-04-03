d5c53c4926aeb51246ca351b0d69f6d98986edb2
Marin Jurjevic <croatiathebest@gmail.com>
2025-04-03 17:35:26 +0200

    replaced array with dict for debuffs

--- level/scripts/level_state.gd
+++ level/scripts/level_state.gd
@@ -1,20 +1,13 @@
 extends Node
 class_name LevelStateSingletone
 
-var active_debuffs: Array[BaseDebuff] = [] 
+var active_debuffs: Dictionary[String, BaseDebuff] = {}
 
 func reset():
 	active_debuffs.clear()
 
 func add_debuff(debuff: BaseDebuff):
-	active_debuffs.append(debuff)
+	active_debuffs[debuff.name] = debuff
 
 func get_debuff_or_null(debuff_type: String) -> BaseDebuff:
-	for debuff in active_debuffs:
-		if debuff.name == debuff_type:
-			return debuff
-	return null
-
-func apply_debuffs_to(target):
-	for debuff in active_debuffs:
-		debuff.apply_effect(target)
+	return active_debuffs.get(debuff_type)
